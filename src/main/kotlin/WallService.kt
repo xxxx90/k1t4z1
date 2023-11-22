d32c9cb04d2e0d4c3c9e00f981fcef8519cc596d
V.Golovchenko <xxxx90@mail.ru>
2023-11-22 16:15:02 +0300

    Task2

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,10 +1,26 @@
 object WallService {
-
+    var idCounter = 1
     private var posts = emptyArray<Post>()
+    private var id = 0
+    fun add(post: Post): Post {
+        posts += post
 
-fun add (post: Post): Post {
-    posts +=post
-    return posts.last()
-}
+        id = idCounter
+        idCounter = idCounter + 1
+        return posts.last()
+    }
+
+    fun update(id: Int): Boolean {
+        for ((index, posts) in posts.withIndex()) {
+            if (posts.id == id) {
+posts.text="изменено"
+
+                return true
+            }
+        }
+
+        return false
+    }
+
 
 }
